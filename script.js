b71203ab299dc702c736938cfe9998ff11bcad3c
tantoquantoanto <168585464+tantoquantoanto@users.noreply.github.com>
2024-05-21 17:26:31 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -19,4 +19,14 @@ for(let i = 0; i<= array.lenght; i++) {
 console.log(`${arrayMax}`)
 
 
-   
+const array2 = [1, 2, 4, 8, 10, 22];
+let array2Sum = 0;
+
+for(let i = 0; i <= array2.lenght; i++) {
+    if(array2[i] %2 === 0) {
+        array2Sum += array2[i];
+
+    }
+}
+
+   console.log(array2Sum)
